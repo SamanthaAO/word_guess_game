a48a9ca0886182e2789b5e25ac1a3196db0db7fa
SamanthaAO <samanthaaorcutt@gmail.com>
2019-04-08 21:14:52 -0700

    most of javascript done with exception of vistory images and such

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,13 +2,12 @@
 
 var countries = ["BRAZIL", "USA", "ENGLAND","CROATIA","GHANA"];
 
-for(var i=0; i<countries.length; i++){
-console.log(countries[i])
-};
-
-console.log(countries);
-
 //computer selects random number which choses random choice of country from list
+var turns = 8;
+var wins = 0;
+var losses = 0;
+
+
 
 var randomNumber = Math.floor(Math.random()*countries.length);
 wordChoice = countries[randomNumber];
@@ -20,63 +19,116 @@ console.log(wordChoice);
 var blankWord = [];
 
 for(var i=0;i<wordChoice.length; i++){
-blankWord.push("_ ")
+blankWord.push("_")
 };
+//want to display it without commas
 console.log(blankWord);
 
 
-//computer stores every letter in word in an array
+//store every letter in word in an array
 
 var wordLetters = wordChoice.split('');
 console.log(wordLetters);
 
+var incorrectGuesses = [];
+var correctLetterTally = 0;
 
 //player choses letter 
 
-var turns = 15;
-var incorrectGuesses = []
 
-document.onkeyup = function(){
-    var userGuess = String.fromCharCode(event.keyCode).
-    toUpperCase();
-    
 
-    console.log(userGuess);
-    console.log(turns);
 
-    //letter chosen by player is compared to letters in array from wordChoice.  
-    for(var i=0; i<wordLetters.length; i++){
+    document.onkeyup = function(){
+        var userGuess = String.fromCharCode(event.keyCode).
+        toUpperCase();
 
-        //If letter matches then take numbers in array where letters match and replace the blank with that number with that letter.
-        if(wordLetters[i].includes(userGuess)){
-            blankWord.splice(i,1,userGuess);
-            console.log(blankWord);
+        console.log(userGuess);
+        console.log(turns);
+
+        //letter chosen by player is compared to letters in array from wordChoice.  
+        for(var i=0; i<wordLetters.length; i++){
+
+            //If letter matches then take numbers in array where letters match and replace the blank with that number with that letter.
+            if(wordLetters[i].includes(userGuess)){
+                blankWord.splice(i,1,userGuess);
+                //increase correctLetterrTally by 1 for each correct letter
+                correctLetterTally++;
+                console.log(blankWord);
+                document.querySelector('#blankWord').innerHTML = blankWord;   
+            }
+            
+        }
+        //If letter does not match letter in word. then letter is shown in box of chosen letters at bottom of screen. 
+        if(wordLetters.includes(userGuess)=== false && incorrectGuesses.includes(userGuess)===false) {
+            incorrectGuesses.push(userGuess);
+            console.log(incorrectGuesses);
+            document.querySelector('#incorrectGuesses').innerHTML = incorrectGuesses;
+                // turns decreases by 1
+                turns--;
+                document.querySelector('#turns').innerHTML = turns;
+        }
+        //if you win
+        if(correctLetterTally === wordLetters.length){
+            wins++;
+            document.querySelector('#wins').innerHTML = wins;
+
+            //insert changing picture and such here!!!!!!
+
+                //reset game
+                randomNumber = Math.floor(Math.random()*countries.length);
+                wordChoice = countries[randomNumber];
+                    console.log(wordChoice);
+
+                wordLetters = wordChoice.split('');
+
+                blankWord.length = 0;
+
+                    for(var i=0;i<wordChoice.length; i++){
+                    blankWord.push("_")
+                    };
+
+                incorrectGuesses.length = 0;
+
+                correctLetterTally = 0;
+                turns=8;
+                
+        }
+
+        //if you lose
+        if(turns <= 0){
+            losses++;
+            document.querySelector('#losses').innerHTML = losses;
+                //reset game
+                randomNumber = Math.floor(Math.random()*countries.length);
+                wordChoice = countries[randomNumber];
+                    console.log(wordChoice);
+
+                wordLetters = wordChoice.split('');
+
+                blankWord.length = 0;
+
+                    for(var i=0;i<wordChoice.length; i++){
+                    blankWord.push("_")
+                    };
+
+                incorrectGuesses.length = 0;
+
+                correctLetterTally = 0;
+                turns=8;
+                //insert sad image for losing
         }
         
     }
-    //If letter does not match letter in word. then letter is shown in box of chosen letters at bottom of screen. 
-    if(wordLetters.includes(userGuess)=== false) {
-        incorrectGuesses.push(userGuess);
-        console.log(incorrectGuesses);
-    }
-    //turns decreases by 1
-    turns--;
-}
 
 
 
-//If all letters are guessed then picture changes, winns++, and song it played. 
+
+
+
+
 //Create object for each countries victory. include picture of flag + national anthem.
 
-//game resets turns reset
-
-//if all the choices are made and word is not guessed then game resets, loses++.
-
-
-
-// how to display in HTML
-// js: document.getElementById("demo").innerHTML = n;
-// html: <p id="demo"></p>
+var victoryArray = 
 
 
 
